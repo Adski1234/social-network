be1ff1cd4b97109b14fd890ae3aaf342b5253cac
ADAM RICHARDSON <ADAM.RICHARDSON123@GMAIL.COM>
2026-03-13 15:49:36 +0000

    more clerk fixing

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <main className='p-8'>
       <h1 className='text-3xl font-bold mb-4'>Social Network</h1>
-      <Show when='unauthenticated'>
+      <Show when='signed-out'>
         <p className='mb-4'>Please sign in to continue.</p>
-        <SignInButton />
+        <SignInButton mode='modal' />
       </Show>
-      <Show when='authenticated'>
+      <Show when='signed-in'>
         <p className='mb-4'>Welcome!</p>
         <nav className='flex gap-4 mt-4'>
           <Link href='/profile'>My Profile</Link>
@@ -21,3 +21,4 @@ export default function Home() {
     </main>
   );
 }
+
